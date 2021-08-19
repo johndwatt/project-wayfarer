d26eb7b5a04cc7609a6180aee74495f72eba95f9
Username <jwaffensmith@gmail.com>
2021-08-18 19:26:25 -0700

    home route set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return HttpResponse("You made it!")
