2a29ec4f82b6ae17724ca1ed285a7c42971952d7
Username <jwaffensmith@gmail.com>
2021-08-19 12:37:03 -0700

    sign in modal renders form

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from .models import User
 from django.views.generic.base import TemplateView
+from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,7 +11,7 @@ class Home(TemplateView):
     template_name = "home.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['user'] = User.objects.all()
+        context['login_form'] = AuthenticationForm()
         return context
 
 class Profile(TemplateView):
