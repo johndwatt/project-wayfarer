702d2467a4c882b157a9394dbddb6a8a539f0cee
Username <jwaffensmith@gmail.com>
2021-08-18 20:06:25 -0700

    added basic html to home page

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse("You made it!")
+class Home(TemplateView):
+    template_name = "home.html"
