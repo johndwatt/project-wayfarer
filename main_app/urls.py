29571106204a40f27bdf3edc21f61d5d2ad27399
johndwatt <jdw23@protonmail.com>
2021-08-19 15:19:56 -0700

    added email to signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, Profile, About, PostDetail
+from .views import Home, Profile, About, PostDetail, ProfileUpdate
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('profile/', Profile.as_view(), name="profile"),
+    path('profile/update/', ProfileUpdate.as_view(), name="profile_update"),
     path('about/', About.as_view(), name="about"),
     path('accounts/signup/', Home.as_view(), name="signup"),
     path('post/<int:pk>', PostDetail.as_view(), name="post-detail"),
