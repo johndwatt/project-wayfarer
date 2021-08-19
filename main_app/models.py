e2d8746c39b23bc52923026e3d2b2511efa7577c
pandaOnCode <asing111@ucsc.edu>
2021-08-18 20:04:58 -0700

    migrated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.db.models import CharField, TextField, ForeignKey, DateTimeField, Model
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    current_city = CharField(max_length=100, default="NA")
+    image = CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class City(Model):
+    name = CharField(max_length=100, default="NA")
+    country = CharField(max_length=100, default="NA")
+    picture = CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(Model):
+    title = CharField(max_length=100, default="NA")
+    content = TextField(max_length=700)
+    city = ForeignKey(City, on_delete=models.CASCADE, related_name='posts')
+    created_at = DateTimeField(auto_now_add=True)
+    user = ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
